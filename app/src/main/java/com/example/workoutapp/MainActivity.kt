96863230de1b33d705dca9fad17b9fcd7f54d0e4
Darfune <harolep@gmail.com>
2022-03-13 16:43:00 +0200

    Main Activity (using view Binding)

--- app/src/main/java/com/example/workoutapp/MainActivity.kt
+++ app/src/main/java/com/example/workoutapp/MainActivity.kt
@@ -2,18 +2,28 @@ package com.example.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.FrameLayout
 import android.widget.Toast
+import com.example.workoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val startFrameLayout: FrameLayout = findViewById(R.id.startFrameLayout)
-        startFrameLayout.setOnClickListener{
+        binding?.startFrameLayout?.setOnClickListener{
             Toast.makeText(this@MainActivity, "Here we will start the exercise", Toast.LENGTH_SHORT).show()
         }
 
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
+    }
+
 }
