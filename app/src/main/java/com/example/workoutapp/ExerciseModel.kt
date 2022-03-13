fc36d6f4a25f1f0a5ff8cfae3f3dc63610d566ad
Darfune <harolep@gmail.com>
2022-03-13 21:57:44 +0200

    Exercises model class

---
+++ app/src/main/java/com/example/workoutapp/ExerciseModel.kt
@@ -0,0 +1,14 @@
+package com.example.workoutapp
+
+class ExerciseModel(
+    private var id: Int,
+    private var name: String,
+    private var image: Int,
+    private var isCompleted: Boolean,
+    private var isSelected: Boolean
+)
+{
+
+
+
+}
