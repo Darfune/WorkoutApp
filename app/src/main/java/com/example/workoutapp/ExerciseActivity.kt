2f4dcdb5ccaa392b2897e2edf00c169a610e8639
Darfune <harolep@gmail.com>
2022-03-14 12:57:57 +0200

    Exercise Activity

--- app/src/main/java/com/example/workoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/example/workoutapp/ExerciseActivity.kt
@@ -5,12 +5,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
 import android.view.View
 import android.widget.Toast
 import com.example.workoutapp.databinding.ActivityExerciseBinding
+import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.random.Random
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var binding: ActivityExerciseBinding?= null
 
@@ -22,6 +25,8 @@ class ExerciseActivity : AppCompatActivity() {
     private var selected: Int = 1
 
     private var counter: Int = 0
+
+    private var textToSpeech: TextToSpeech? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +42,8 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.exerciseToolbar?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        textToSpeech = TextToSpeech(this, this)
 
         exerciseList = Constants.defaultExerciseList()
 
@@ -66,6 +73,8 @@ class ExerciseActivity : AppCompatActivity() {
     private fun setupRestView(){
         if (counter >= 7){
             Toast.makeText(this@ExerciseActivity, "You Finished", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
         else{
             if (restTimer != null){
@@ -91,12 +100,12 @@ class ExerciseActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     private fun setExerciseImage(flag: Boolean) {
         if (flag){
-            binding?.titleTextView?.text = "" +  exerciseList!![selected].getName()
+            binding?.titleTextView?.text = exerciseList!![selected].getName()
             binding?.upcomingExerciseTopTextView?.visibility = View.INVISIBLE
             binding?.upcomingExerciseBottomTextView?.visibility = View.INVISIBLE
             binding?.exerciseImageView?.setImageResource(exerciseList!![selected].getImage())
             binding?.exerciseImageView?.visibility = View.VISIBLE
-
+            textToSpeechFunction(exerciseList!![selected].getName())
         }
         else{
             do {
@@ -109,14 +118,39 @@ class ExerciseActivity : AppCompatActivity() {
             binding?.upcomingExerciseBottomTextView?.text = exerciseList!![selected].getName()
             binding?.upcomingExerciseBottomTextView?.visibility = View.VISIBLE
             binding?.titleTextView?.text = "Rest"
+            textToSpeechFunction("Rest")
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
+        if (restTimer != null){
+            restTimer?.cancel()
+            timerProgress = 0
+
+        }
+
+        if (textToSpeech != null){
+            textToSpeech!!.stop()
+            textToSpeech!!.shutdown()
+        }
 
         binding = null
     }
 
+    override fun onInit(p0: Int) {
+        if (p0 == TextToSpeech.SUCCESS){
+            val result = textToSpeech?.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                || result == TextToSpeech.LANG_NOT_SUPPORTED)
+                    Toast.makeText(this@ExerciseActivity, "The Language for TextToSpeech is not supported!", Toast.LENGTH_SHORT).show()
+        }else
+            Toast.makeText(this@ExerciseActivity, "TextToSpeech initialization Failed!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun textToSpeechFunction(text: String){
+        textToSpeech!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
 }
